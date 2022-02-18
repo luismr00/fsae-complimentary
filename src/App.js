3b793d31c3dba14058567e9065b136998b001dee
Luis Rangel <night@Luiss-MBP.lan>
2022-02-17 20:56:42 -0800

    initial charts added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Chart from './components/Chart';
+import Pie from './components/Pie';
+import LineChart from './components/LineChart';
 
 function App() {
   return (
@@ -7,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Add your graphs below.
         </p>
         <a
           className="App-link"
@@ -18,6 +21,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Graphs goes here!</p>
+      <Chart />
+      <Pie />
+      <LineChart />
     </div>
   );
 }
